a1eec2b8facbdd2ca227ae3e57d3bf1667cdf2cb
Antônio Luis <antonioluisp97@gmail.com>
2020-12-15 18:42:44 -0300

    finalizacao do back end

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const routes = require('./routes');// importa as rotas do arquivo
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
